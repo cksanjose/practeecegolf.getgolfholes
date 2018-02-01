76948c189a25c9c5f08ff312709e5605bd95df9d
Cesar San Jose <cksanjose@gmail.com>
2018-02-01 10:51:11 -0600

    Update ReadMe.md

---
+++ Practeece.GolfHoles/models/GolfHole.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practeece.GolfHoles.models
+{
+    public class GolfHole
+    {
+        public int HoleId { get; set; }
+
+        public int Distance { get; set; }
+
+        public int Par { get; set; }
+
+        public string Skill { get; set; }
+
+        public string Hazard { get; set; }
+
+        public int AverageSwingsToGreen { get; set; }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # practeecegolf.getgolfholes
-Azure Function to return a list of golf holes by skill level
+Azure Functions to return a list of golf holes by skill level and a list of golf swing results.
